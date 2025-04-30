7324458c5d39323b05891226d5594b4debd35cb8
Gabriel-Lemus <gabriel_lemus17@icloud.com>
2025-04-29 19:54:50 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+  const reveals = document.querySelectorAll('.reveal');
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = el.getBoundingClientRect().top;
+    if (revealTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+});
